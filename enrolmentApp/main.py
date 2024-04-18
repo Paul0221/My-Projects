5736ddb9401815c034542e0fe378a94085140507
Paul Omara Odida <sc20poo@leeds.ac.uk>
2024-04-18 01:56:09 +0100

    Reverted back to binomial approach as it is more suitable for keyphrase analysis

--- enrolmentApp/main.py
+++ enrolmentApp/main.py
@@ -16,12 +16,20 @@ import nltk
 from nltk.corpus import stopwords
 from rake_nltk import Rake
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 #nltk.download()
 >>>>>>> 1412968 (Test of rake_nltk able to process sample statement into keyphrases)
 =======
 from sklearn.model_selection import train_test_split
 >>>>>>> e8aaad4 (Added initial training and test sets)
+=======
+from itertools import combinations
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+>>>>>>> b668329 (Reverted back to binomial approach as it is more suitable for keyphrase analysis)
 
 app = FastAPI()
 
@@ -439,6 +447,7 @@ async def course_selection(request: Request):
     return templates.TemplateResponse("course_selection.html", {"request": request, "select_uni": select_uni,
                                                                 "select_course": select_course})
 
+
 @app.post("/submit_course_selection")
 async def submit_course_selection(select_uni: str = Form(...), select_course: str = Form(...)):
     ''' cursor = conn.cursor()
@@ -473,6 +482,7 @@ async def submit_course_selection(select_uni: str = Form(...), select_course: st
     nltk.download('punkt')
     nltk.download('stopwords')
 
+
     test_keywords = """\
     skilled programmer
     creative thinker
@@ -492,7 +502,7 @@ async def submit_course_selection(select_uni: str = Form(...), select_course: st
 
     keyword_array = [keyword.strip() for keyword in keyword_array]
 
-    sample_statement = """ My fascination with technology was sparked when, as a child I thought it would be a great idea to take apart my Playstation console. Aware of the danger, I was still eager to see how it all worked inside. I find it intriguing how fast society has been shaped and continues to be, by the influence of Computer Science. A few years ago if someone were to have claimed that cars would become autonomous, people would have doubted them. Now we are at a stage where nearly anything is possible and this is due to the relentless problem solving of computer scientists. The latest software update released by Tesla motors allows their cars to learn how to drive themselves, and is an example of artificial intelligence, a sector which I am most interested in. I want to study Computer Science because I want to gain the knowledge needed to be able to help find solutions to world problems, with the efficient use of computer technology. With the knowledge and skills, I will attain from this course, the creative ideas that I could bring into fruition would be endless. I hope to become one of the computer scientists who adapt technology to help the human race evolve. One possibility would be for artificially intelligent gadgets to recognise different people and adjust to their individual needs based on personal preferences. I have been teaching myself Objective-C syntax in my spare time and have completed online programming courses, which have allowed me to explore the endless possibilities that computer science can bring to the world. I have also learnt to create a simple iOS game using Apple’s syntax called ‘Swift’, in XCode alongside Photoshop.
+    sample_statement_one = """ My fascination with technology was sparked when, as a child I thought it would be a great idea to take apart my Playstation console. Aware of the danger, I was still eager to see how it all worked inside. I find it intriguing how fast society has been shaped and continues to be, by the influence of Computer Science. A few years ago if someone were to have claimed that cars would become autonomous, people would have doubted them. Now we are at a stage where nearly anything is possible and this is due to the relentless problem solving of computer scientists. The latest software update released by Tesla motors allows their cars to learn how to drive themselves, and is an example of artificial intelligence, a sector which I am most interested in. I want to study Computer Science because I want to gain the knowledge needed to be able to help find solutions to world problems, with the efficient use of computer technology. With the knowledge and skills, I will attain from this course, the creative ideas that I could bring into fruition would be endless. I hope to become one of the computer scientists who adapt technology to help the human race evolve. One possibility would be for artificially intelligent gadgets to recognise different people and adjust to their individual needs based on personal preferences. I have been teaching myself Objective-C syntax in my spare time and have completed online programming courses, which have allowed me to explore the endless possibilities that computer science can bring to the world. I have also learnt to create a simple iOS game using Apple’s syntax called ‘Swift’, in XCode alongside Photoshop.
 
 I understand that computer science is not just about programming and hardware but also about the ethics entailed in the process of a design as well as innovative thinking. Studying A-Level Philosophy and Ethics has given me an insight into many ethical situations that may arise around computer science such as the creation of artificial intelligence. This raises controversy of whether or not we should be trying to create artificial intelligence, as people have different beliefs and faiths. One example of such controversy was during the development of Honda’s Asimo robot when engineers had to visit the Vatican to seek permission to continue the project due to how human-like the robot was developed to walk. During a week of volunteering on NCS I visited Wazoku, the creators of an idea-sharing software used by major corporations such as Waitrose and The BBC. I was given an insight into the working environment that I hope to join after completing my degree. I am applying for work experience at Wazoku, in order to learn about how the software industry can influence the progress of companies.
 
@@ -502,11 +512,11 @@ Technological advancements take place around us everyday, from the evolution of 
 
     # preprocess the statement
 
-    processed_text = sample_statement.lower().replace(".", "").replace(",", "").replace("'", "")
+    processed_text = sample_statement_one.lower().replace(".", "").replace(",", "").replace("'", "")
 
     # get stop words
 
-    stop_words = set(stopwords.words('english'))
+    set(stopwords.words('english'))
 
     r_nltk = Rake()
 
@@ -516,60 +526,33 @@ Technological advancements take place around us everyday, from the evolution of 
 
     print("keyword_array = ", keyword_array)
 
-    train_set, test_set = train_test_split(keywords, test_size=0.9, random_state=43)
+    # removed train_test_split as this is inappropriate for binomial approach
 
-    true_pos = 0
-    true_neg = 0
-    false_pos = 0
-    false_neg = 0
+    correct = 0
 
     score = 0
 
-    for keyphrase in test_set:
+    total = 0
+
+    for keyphrase in keywords:
         found = False
         for keyword in keyword_array:
             if keyword in keyphrase:
                 found = True
                 break
-        if found == True:
-            if keyword in keyphrase:
-                true_pos += 1
-                score += 1
-            else:
-                false_pos += 1
-        else:
-            if keyword in keyphrase:
-                false_neg += 1
-
-    total = true_pos + true_neg + false_pos + false_neg
+        if found:
+            correct += 1
+            score += 1
+        total += 1
 
     if total == 0:
         accuracy = 0
     else:
-        accuracy = true_pos + true_neg / total
-    if true_pos + false_pos == 0:
-        precision = 0
-    else:
-        precision = true_pos / (true_pos + false_pos)
-    if true_pos + false_neg == 0:
-        recall = 0
-    else:
-        recall = true_pos / (true_pos + false_neg)
-    if true_neg + false_pos == 0:
-        specificity = 0
-    else:
-        specificity = true_neg / (true_neg + false_pos)
-    if precision + recall == 0:
-        f1_score = 0
-    else:
-        f1_score = 2 * (precision * recall) / (precision + recall)
+        accuracy = (correct / total) * 100
 
-    print("Accuracy = ", accuracy)
-    print("Precision = ", precision)
-    print("Recall = ", recall)
-    print("Specificity = ", specificity)
-    print("F1 Score = ", f1_score)
-    print("Personal statement score =", score)
+    print("Accuracy = ", accuracy, "%")
+    print("Total = ", total)
+    print("Personal statement score = ", score)
 
     return {"message": "Success: required fields are correctly processed"}
     # else:
